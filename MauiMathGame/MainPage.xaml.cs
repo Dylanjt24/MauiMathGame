9f483401e4684dd640d89011088194963ff2f931
Dylan Thierbach <44076048+Dylanjt24@users.noreply.github.com>
2024-03-04 09:24:03 -0800

    Added Clicked event to addition button, created OnGameChosen signature

--- MauiMathGame/MainPage.xaml.cs
+++ MauiMathGame/MainPage.xaml.cs
@@ -1,11 +1,15 @@
-﻿namespace MauiMathGame
+﻿namespace MauiMathGame;
+
+public partial class MainPage : ContentPage
 {
-    public partial class MainPage : ContentPage
+    public MainPage()
     {
-        public MainPage()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
     }
 
+    //send object has information about the object, EventArgs is a .NET class that has information about the event
+    private void OnGameChosen(object send, EventArgs e)
+    {
+
+    }
 }
