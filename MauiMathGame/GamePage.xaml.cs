528d19cd39a4da22e9ba48dac9b6028088196dcc
Dylan Thierbach <44076048+Dylanjt24@users.noreply.github.com>
2024-03-06 09:23:28 -0800

    Created BindingContext for GamePage class, binded GameType field to text in the label

--- MauiMathGame/GamePage.xaml.cs
+++ MauiMathGame/GamePage.xaml.cs
@@ -2,8 +2,13 @@ namespace MauiMathGame;
 
 public partial class GamePage : ContentPage
 {
+    public string GameType { get; set; }
     public GamePage(string gameType)
     {
         InitializeComponent();
+        GameType = gameType;
+        // Need BindingContext in order to assign constructor gameType to the incoming GameType
+        // "this" assigns the BindingContext to the current instance of this class
+        BindingContext = this;
     }
 }
