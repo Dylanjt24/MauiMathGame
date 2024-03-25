1baac322dc631840cc8bb62c8a5d999461ec4e88
Dylan Thierbach <44076048+Dylanjt24@users.noreply.github.com>
2024-03-25 09:22:55 -0700

    Created Models folder and Game model

---
+++ MauiMathGame/Models/Game.cs
@@ -0,0 +1,20 @@
+﻿using Windows.Gaming.XboxLive.Storage;
+
+namespace MauiMathGame.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public GameSaveOperationResult Type { get; set; }
+        public int Score { get; set; }
+        public DateTime DatePlayed { get; set; }
+    }
+
+    public enum GameOperation
+    {
+        Addition,
+        Subtraction,
+        Multiplication,
+        Division,
+    }
+}
