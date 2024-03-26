cfb7724696a1dcdf75ef008c53da7b7368f7cead
Dylan Thierbach <44076048+Dylanjt24@users.noreply.github.com>
2024-03-26 09:25:17 -0700

    Installed SQLite package and added db attributes to Game class

--- MauiMathGame/Models/Game.cs
+++ MauiMathGame/Models/Game.cs
@@ -1,11 +1,14 @@
-﻿using Windows.Gaming.XboxLive.Storage;
+﻿using SQLite;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MauiMathGame.Models
 {
+    [Table("game")]  // Table attribute not necessary since the class is already named "Game", just for example purposes
     public class Game
     {
+        [PrimaryKey, AutoIncrement, Column("Id")]
         public int Id { get; set; }
-        public GameSaveOperationResult Type { get; set; }
+        public GameOperation Type { get; set; }
         public int Score { get; set; }
         public DateTime DatePlayed { get; set; }
     }
