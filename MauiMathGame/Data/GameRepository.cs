798a1e0fe3cb00395db52613baa92ec28b186321
Dylan Thierbach <44076048+Dylanjt24@users.noreply.github.com>
2024-03-27 09:26:06 -0700

    Created initial GameRepository class

---
+++ MauiMathGame/Data/GameRepository.cs
@@ -0,0 +1,20 @@
+﻿using SQLite;
+
+namespace MauiMathGame.Data
+{
+    public class GameRepository
+    {
+        string _dbPath; // Where database lives in file system
+        private SQLiteConnection conn;
+
+        public GameRepository(string dbPath)
+        {
+            _dbPath = dbPath;
+        }
+
+        public void Init()
+        {
+
+        }
+    }
+}
