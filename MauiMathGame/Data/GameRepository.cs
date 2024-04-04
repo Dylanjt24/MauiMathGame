4c309c6a83a2ab8c9319fe51824c7e9300fc3e43
Dylan Thierbach <44076048+Dylanjt24@users.noreply.github.com>
2024-04-04 09:22:21 -0700

    Created GetAllGames method to retrieve list of played games

--- MauiMathGame/Data/GameRepository.cs
+++ MauiMathGame/Data/GameRepository.cs
@@ -18,5 +18,11 @@ namespace MauiMathGame.Data
             conn = new SQLiteConnection(_dbPath);
             conn.CreateTable<Game>();  // Creates a table based on the specified Game model - model properties will become columns for the table
         }
+
+        public List<Game> GetAllGames()
+        {
+            Init(); // Run Init method to create the table if it doesn't already exist
+            return conn.Table<Game>().ToList(); // Retrieve games from table and convert it to a list
+        }
     }
 }
