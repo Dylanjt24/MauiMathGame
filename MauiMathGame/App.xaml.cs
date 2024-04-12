c00ab17e2f8912b965dcda3acf12d48828c48e47
Dylan Thierbach <44076048+Dylanjt24@users.noreply.github.com>
2024-04-12 09:22:19 -0700

    Injected GameRepository, created GameRepository field

--- MauiMathGame/App.xaml.cs
+++ MauiMathGame/App.xaml.cs
@@ -1,12 +1,17 @@
-﻿namespace MauiMathGame
+﻿using MauiMathGame.Data;
+
+namespace MauiMathGame
 {
     public partial class App : Application
     {
-        public App()
+        public static GameRepository GameRepository { get; private set; }
+        public App(GameRepository gameRepository)
         {
             InitializeComponent();
 
             MainPage = new AppShell();
+
+            GameRepository = gameRepository;
         }
     }
 }
